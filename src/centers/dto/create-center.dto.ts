65e9da1b25e6498582394a3539f47e04bd04d354
JOSE MARTIN PARDO MORALES <72662638>
2026-07-01 14:29:14 -0500

    update0229010726

--- src/centers/dto/create-center.dto.ts
+++ src/centers/dto/create-center.dto.ts
@@ -1,21 +1,45 @@
-import { IsString, IsDecimal, IsInt, IsBoolean, IsOptional } from 'class-validator';
+import { IsString, IsNumber, IsInt, IsOptional, IsUrl } from 'class-validator';
 
 export class CreateCenterDto {
   @IsString()
-  name!:string;
+  name?:string;
 
   @IsString()
-  address!:string;
-
-  @IsDecimal()
-  lat!:number;
-
-  @IsDecimal()
-  lng!:number;
+  state?:string;
 
   @IsString()
-  contact_info!:string;
+  city?:string;
 
+  @IsString()
+  address?:string;
+
+  // Si el frontend envía un teléfono o no, lo aceptamos
+  @IsOptional()
+  @IsString()
+  phone?:string; 
+
+  @IsOptional()
+  @IsString()
+  hours?:string;
+
+  @IsOptional()
+  @IsString()
+  source?:string;
+
+  @IsOptional()
+  @IsUrl() // Valida que si mandan URL, tenga formato correcto
+  url?:string;
+
+  // Usamos IsNumber para aceptar floats reales del JSON (ej: 10.5133)
+  @IsOptional()
+  @IsNumber()
+  lat?:number;
+
+  @IsOptional()
+  @IsNumber()
+  lng?:number;
+
+  @IsOptional()
   @IsInt()
   capacity_per_day?:number;
 }
